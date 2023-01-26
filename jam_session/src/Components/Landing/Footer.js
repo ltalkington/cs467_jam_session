dd44e4aa4a8f0fb7b1fe4ce89a0dec8ca586549e
Logan Talkington <talkingl@oregonstate.edu>
2023-01-25 19:41:11 -0600

    adding react app and landing screen

---
+++ jam_session/src/Components/Landing/Footer.js
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
+export default function Footer() {
+  return (
+    <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="facebook-f" />
+          </a>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="twitter" />
+          </a>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="google" />
+          </a>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="instagram" />
+          </a>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="linkedin" />
+          </a>
+          <a href="" className="me-4 text-reset">
+            <MDBIcon color="secondary" fab icon="github" />
+          </a>
+        </div>
+      </section>
+
+      <section className="">
+        <MDBContainer className="text-center text-md-start mt-5">
+          <MDBRow className="mt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <MDBIcon color="secondary" icon="gem" className="me-3" />
+                Jam Sessions
+              </h6>
+              <p>Welcome to Jam Sessions.</p>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Application</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Login
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Register
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Login
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Register
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>Corvallis, OR</p>
+              <p>fantasysports@gmail.com</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2023 Copyright:
+        <a className="text-reset fw-bold" href="#">
+          jamsessions.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
---
+++ jam_session/src/Components/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+
+function CollapsibleExample() {
+  const handlePricingClickScroll = () => {
+    const element = document.getElementById("pricing");
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const handleFeaturesClickScroll = () => {
+    const element = document.getElementById("features");
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  return (
+    <Navbar collapseOnSelect expand="lg" id="navbar-landing">
+      <Container>
+        <Navbar.Brand class="nav-text" href="#home">
+          Jam Session
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={handleFeaturesClickScroll}>Features</Nav.Link>
+            <Nav.Link onClick={handlePricingClickScroll}>Demo</Nav.Link>
+          </Nav>
+          <Nav>
+            <Button id="sign-in-button" variant="">
+              Login{" "}
+            </Button>
+            <br></br>
+            <Button id="register-button" variant="outline-success">
+              Register
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
