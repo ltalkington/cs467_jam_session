a26e177ad9d44785d69b8f7187542fa63032b218
Mike <clancymi@oregonstate.edu>
2023-02-13 16:34:33 -0500

    Adds setting page

--- jam_session_front_end/src/App.js
+++ jam_session_front_end/src/App.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress"
-import {AuthenticationGuard} from "./Components/Authenticate/AuthenticationGuard";
+import { AuthenticationGuard } from "./Components/Authenticate/AuthenticationGuard";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import NotFound from "./Pages/Application/404.js";
 import JamSession from "./Pages/Application/JamSession.js";
@@ -17,60 +17,60 @@ import Timeline from "./Pages/Application/Timeline.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const { isLoading } = useAuth0();
+    const { isLoading } = useAuth0();
 
-  if (isLoading) {
-      return (
-          <div
-              style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-              }}
-              className="page-layout">
-              <CircularProgress />
-          </div>
-      );
-  }
+    if (isLoading) {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                className="page-layout">
+                <CircularProgress />
+            </div>
+        );
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header2">
-          <Routes>
-            <Route path="/" exact element={<LandingPage/>}/>
-            <Route
-                path="/profile"
-                exact element={ <AuthenticationGuard component={Profile} />}
-            />
-            <Route
-                path="/timeline"
-                exact element={<AuthenticationGuard component={Timeline} />}
-            />
-            <Route
-                path="/posts"
-                exact element={<AuthenticationGuard component={Posts} />}
-            />
-            <Route
-                path="/jamsessions"
-                exact element={<AuthenticationGuard component={JamSession} />}
-            />
-            <Route
-                path="/messages"
-                exact element={ <AuthenticationGuard component={Messages} />}
-            />
-            <Route
-                path="/settings"
-                exact element={ <AuthenticationGuard component={Settings} />}
-            />
-            <Route
-              path="/searchresults"
-              exact element={<AuthenticationGuard component={SearchResults} />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header2">
+                <Routes>
+                    <Route path="/" exact element={<LandingPage />} />
+                    <Route
+                        path="/profile"
+                        exact element={<AuthenticationGuard component={Profile} />}
+                    />
+                    <Route
+                        path="/timeline"
+                        exact element={<AuthenticationGuard component={Timeline} />}
+                    />
+                    <Route
+                        path="/posts"
+                        exact element={<AuthenticationGuard component={Posts} />}
+                    />
+                    <Route
+                        path="/jamsessions"
+                        exact element={<AuthenticationGuard component={JamSession} />}
+                    />
+                    <Route
+                        path="/messages"
+                        exact element={<AuthenticationGuard component={Messages} />}
+                    />
+                    <Route
+                        path="/settings"
+                        exact element={<AuthenticationGuard component={Settings} />}
+                    />
+                    <Route
+                        path="/searchresults"
+                        exact element={<AuthenticationGuard component={SearchResults} />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- jam_session_front_end/src/Pages/Application/Settings.js
+++ jam_session_front_end/src/Pages/Application/Settings.js
@@ -1,11 +1,49 @@
 import ResponsiveDrawer from "../../Components/Application/Sidebar/Sidebar.js";
+import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Switch from '@mui/material/Switch';
+
+
+
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'start',
+  color: theme.palette.text.secondary,
+}));
 
 function Settings() {
   return (
     <div>
       <ResponsiveDrawer></ResponsiveDrawer>
 
-      <h1> Settings </h1>
+      <Typography variant="h1" align="center" color="text.white" paragraph>
+        Settings
+      </Typography>
+      <Typography variant="p" align="center" color="text.white" paragraph>
+        Use the switches below to turn your user settings on and off.
+      </Typography>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box sx={{ width: '40%' }} >
+          <Stack spacing={1}
+            sx={{ justifyContent: 'space-between' }}>
+            <Item>Setting Number 1 <Switch /></Item>
+            <Item>Setting Number 2 <Switch /></Item>
+            <Item>Setting Number 3 <Switch /></Item>
+          </Stack>
+        </Box>
+      </Grid>
     </div>
   );
 }
