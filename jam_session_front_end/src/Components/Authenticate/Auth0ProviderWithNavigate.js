9fbe298a82a4200366db555df0088cf7b5f63dc8
Logan Talkington <talkingl@oregonstate.edu>
2023-02-15 11:43:53 -0600

    updating access token

--- jam_session_front_end/src/Components/Authenticate/Auth0ProviderWithNavigate.js
+++ jam_session_front_end/src/Components/Authenticate/Auth0ProviderWithNavigate.js
@@ -3,30 +3,33 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Auth0ProviderWithNavigate = ({ children }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-    const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
 
-    const onRedirectCallback = (appState) => {
-        navigate(appState?.returnTo || window.location.pathname);
-    };
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
 
-    if (!(domain && clientId && redirectUri)) {
-        return null;
-    }
+  if (!(domain && clientId && redirectUri)) {
+    return null;
+  }
 
-    return (
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            authorizationParams={{
-                redirect_uri: redirectUri,
-            }}
-            onRedirectCallback={onRedirectCallback}
-        >
-            {children}
-        </Auth0Provider>
-    );
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+      s
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
--- jam_session_front_end/src/index.js
+++ jam_session_front_end/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './index.css';
+import "./index.css";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Auth0ProviderWithNavigate} from "./Components/Authenticate/Auth0ProviderWithNavigate";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0ProviderWithNavigate } from "./Components/Authenticate/Auth0ProviderWithNavigate";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Auth0ProviderWithNavigate>
-                <App />
-            </Auth0ProviderWithNavigate>
-        </Router>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
