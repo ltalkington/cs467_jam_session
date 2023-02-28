e18b86e756d052113f4e921c335955c480f1ec9f
Mike <mclancy96@gmail.com>
2023-02-28 12:39:52 -0500

    Updates routes for posts

--- jam_session_back_end/controllers/post_controllers.js
+++ jam_session_back_end/controllers/post_controllers.js
@@ -1,58 +1,59 @@
 const db = require("./db");
 
-async function getMessagesByReceiverId(message_id, res) {
-    const inserts = [message_id];
-    const query = "SELECT * FROM Messages WHERE receiverID= ?;";
+async function getPostsByPosterId(poster_id, res) {
+    const inserts = [poster_id];
+    const query = "SELECT * FROM Posts WHERE poster_id= ?;";
     await db.pool.query(query, inserts, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 }
 
-async function getMessagesBySenderId(message_id, res) {
-    const inserts = [message_id];
-    const query = "SELECT * FROM Messages WHERE senderID= ?;";
+async function getPostById(post_id, res) {
+    const inserts = [post_id];
+    const query = "SELECT * FROM Posts WHERE post_id= ?;";
     await db.pool.query(query, inserts, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 }
 
-async function getMessageById(message_id, res) {
-    const inserts = [message_id];
-    const query = "SELECT * FROM Messages WHERE messageID= ?;";
-    await db.pool.query(query, inserts, function (error, results, fields) {
+async function getPosts(res) {
+    const query = "SELECT * FROM Posts;";
+    await db.pool.query(query, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 }
 
-async function createMessage(inserts, res) {
+async function createPost(inserts, res) {
+    const dateNow = new Date(Date.now());
+    inserts.push(dateNow);
     const query =
-        "INSERT INTO Messages (senderID, receiverID, content) VALUES (?,?,?);";
+        "INSERT INTO Posts (poster_id, portfolio_link, post_type, posted_date) VALUES (?,?,?,?);";
     await db.pool.query(query, inserts, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 }
 
-async function updateMessage(inserts, res) {
+async function updatePost(inserts, res) {
     const query =
-        "UPDATE Messages SET senderID=?, receiverID=?, content=? WHERE messageID=?;";
+        "UPDATE Posts SET poster_id=?, portfolio_link=?, post_type=? WHERE post_id=?;";
     await db.pool.query(query, inserts, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 
 }
 
-async function deleteMessage(inserts, res) {
-    const query = "DELETE FROM Messages WHERE messageID = ?;";
+async function deletePost(inserts, res) {
+    const query = "DELETE FROM Posts WHERE post_id = ?;";
     await db.pool.query(query, inserts, function (error, results, fields) {
         return db.returnCallback(error, results, fields, res);
     });
 }
 
 module.exports = {
-    getMessagesByReceiverId,
-    getMessagesBySenderId,
-    getMessageById,
-    createMessage,
-    updateMessage,
-    deleteMessage,
+    getPostsByPosterId,
+    getPosts,
+    getPostById,
+    createPost,
+    updatePost,
+    deletePost,
 };
--- jam_session_back_end/routes/post_routes.js
+++ jam_session_back_end/routes/post_routes.js
@@ -1,9 +1,9 @@
 /**
- * For routes regarding messages
+ * For routes regarding posts
  */
 const express = require("express");
 const router = express.Router();
-const message_controller = require("../controllers/messages_controllers");
+const post_controller = require("../controllers/post_controllers");
 
 router.use(
     express.urlencoded({
@@ -11,77 +11,78 @@ router.use(
     })
 );
 
-//creates a message
-router.post("/message/new", async function (req, res) {
+//creates a post
+router.post("/post/new", async function (req, res) {
     let inserts = [
-        req.body.senderID,
-        req.body.receiverID,
-        req.body.content
+        req.body.poster_id,
+        req.body.portfolio_link,
+        req.body.post_type
     ];
     try {
-        message_controller.createMessage(inserts, res);
+        post_controller.createPost(inserts, res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
 
-// updates message
-router.put("/message/:id/edit", async function (req, res) {
+// updates post
+router.put("/post/:id/edit", async function (req, res) {
     let inserts = [
-        req.body.senderID,
-        req.body.receiverID,
-        req.body.content,
+        req.body.poster_id,
+        req.body.portfolio_link,
+        req.body.post_type,
         req.params.id
     ];
     try {
-        message_controller.updateMessage(inserts, res);
+        post_controller.updatePost(inserts, res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
 
-//get single message
-router.get("/message/:id", async function (req, res) {
+//get single post
+router.get("/post/:id", async function (req, res) {
     try {
-        message_controller.getMessageById(req.params.id, res);
+        post_controller.getPostById(req.params.id, res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
 
-//deletes a message
-router.delete("/message/:id/delete", async function (req, res) {
+//get all posts
+router.get("/post", async function (req, res) {
     try {
-        message_controller.deleteMessage(req.params.id, res);
+        post_controller.getPosts(res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
 
-// ===================== Routes for Users Regarding Messages ================
-
-//get all messages where the current user is a recipient
-router.get("/user/:id/messages/received", async function (req, res) {
+//deletes a post
+router.delete("/post/:id/delete", async function (req, res) {
     try {
-        message_controller.getMessagesByReceiverId(req.params.id, res);
+        post_controller.deletePost(req.params.id, res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
 
-//get all messages where the current user is a sender
-router.get("/user/:id/messages/sent", async function (req, res) {
+// ===================== Routes for Users Regarding Posts ================
+
+//get all posts where the current user is a recipient
+router.get("/user/:id/posts/", async function (req, res) {
     try {
-        message_controller.getMessagesBySenderId(req.params.id, res);
+        post_controller.getPostsByPosterId(req.params.id, res);
     } catch (error) {
         console.log(JSON.stringify(error));
         res.status(400).send(JSON.stringify(error));
     }
 });
+
 
 module.exports = router;
