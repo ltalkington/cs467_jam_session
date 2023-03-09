8b1298a2833bf1de8d1ddbc65bf4d171675ce288
Mike <clancymi@oregonstate.edu>
2023-03-09 14:26:48 -0500

    Adds env variables to db file

--- jam_session_back_end/controllers/db.js
+++ jam_session_back_end/controllers/db.js
@@ -1,10 +1,12 @@
 var mysql = require("mysql");
+require('dotenv').config();
+
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "jamsession.c4xx6wbnjy4u.us-east-1.rds.amazonaws.com",
-  user: "jammeradmin",
-  password: "JamSession2023",
-  database: "jam_session",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
 });
 
 const returnCallback = (error, results, fields, res) => {
